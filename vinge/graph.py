b05f9f5d3102950a3902168841b71c298e68f0f4
epurdy <epurdy@uchicago.edu>
2012-09-03 11:33:48 -0500

    changes in the graph construction algorithm: tag vertices are no longer shared between log lines, also added edges between adjacent log lines, also added optional edge weight parameters

--- vinge/graph.py
+++ vinge/graph.py
@@ -3,42 +3,42 @@ import networkx as nx
 import numpy as np
 from vertex import UniqueIDVertex, TagVertex
 
-def make_graph(loglines, tag_map, id_map, time_weighting, max_tag_degree=10):
+def make_graph(loglines, tag_map, id_map, time_weighting, adjacent_logline_edge_weight=1.0, logline_id_edge_weight=1.0, logline_tag_edge_weight=1.0):
 
     g = nx.DiGraph()
     g.add_nodes_from(loglines)
+
+    oldll = None
+    for ll in loglines:
+        if oldll is not None:
+            g.add_edge(oldll, ll, weight=adjacent_logline_edge_weight)
+            g.add_edge(ll, oldll, weight=adjacent_logline_edge_weight)
 
     for id in id_map:
         v = UniqueIDVertex(id)
         g.add_node(v)
         for ll in id_map[id]:
-            g.add_edge(ll, v, weight=1.0)
-            g.add_edge(v, ll, weight=1.0)
+            g.add_edge(ll, v, weight=logline_id_edge_weight)
+            g.add_edge(v, ll, weight=logline_id_edge_weight)
 
 
     for tag in tag_map:
         v, oldv = None, None
-        num_ll_neighbors = 0
 
         for ll in tag_map[tag]:
-            # make a newer version of the tag vertex whenever
-            # degree is too high
-            if v is None or num_ll_neighbors >= max_tag_degree:
-                oldv = v
-                v = TagVertex(tag, ll.time)
-                g.add_node(v)
-                num_ll_neighbors = 0
+            oldv = v
+            v = TagVertex(tag, ll.time)
+            g.add_node(v)
 
-                # add edges between adjacent tag vertices, with
-                # weight based on how far apart the times are
-                if oldv is not None:
-                    wt = time_weighting(v.time, oldv.time)
-                    g.add_edge(v, oldv, weight=wt)
-                    g.add_edge(oldv, v, weight=wt)
+            # add edges between adjacent tag vertices, with
+            # weight based on how far apart the times are
+            if oldv is not None:
+                wt = time_weighting(v.time, oldv.time)
+                g.add_edge(v, oldv, weight=wt)
+                g.add_edge(oldv, v, weight=wt)
 
-            g.add_edge(ll, v, weight=1.0)
-            g.add_edge(v, ll, weight=1.0)
-            num_ll_neighbors += 1
+            g.add_edge(ll, v, weight=logline_tag_edge_weight)
+            g.add_edge(v, ll, weight=logline_tag_edge_weight)
                 
     # normalize weights so they sum to 1.0
     for u in g.nodes_iter():
@@ -46,15 +46,13 @@ def make_graph(loglines, tag_map, id_map, time_weighting, max_tag_degree=10):
         for (_,v,edata) in g.edges_iter(u, data=True):
             totwt += edata['weight']
 
-        if totwt > 0.0:
-            itotwt = 1.0 / totwt
-        else:
-            itotwt = 1.0
-
 #        assert(totwt > 0.0)
 
-        for (_,v) in g.edges_iter(u):
-            g[u][v]['weight'] = itotwt * g[u][v]['weight'] 
+        if totwt > 0.0:
+            itotwt = 1.0 / totwt
+
+            for (_,v) in g.edges_iter(u):
+                g[u][v]['weight'] = itotwt * g[u][v]['weight'] 
         
     g = nx.freeze(g)
     return g
