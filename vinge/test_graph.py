b7a9a98010be3b50fb67ea7292b11bf2d7a15b87
epurdy <epurdy@uchicago.edu>
2012-09-02 21:25:20 -0500

    changes to graph testing code

--- vinge/test_graph.py
+++ vinge/test_graph.py
@@ -5,8 +5,8 @@ from parser import parse_log_file
 from graph import make_graph
 
 def time_weighting(t1,t2):
-#    return np.exp(-abs(t2-t1))
-    return 1.0
+    tdelta = -abs(t2-t1)
+    return np.exp(0.0001 * tdelta.total_seconds())
 
 def main():
     (log_line_vertices, tag_map, id_map) = parse_log_file(sys.argv[1])
@@ -17,14 +17,16 @@ def main():
     posn = log_line_vertices[log_line_number]
 
     while True:
-        print str(posn)[:30]
+        print str(posn)[:80]
         print '_' * 10
-        nbrs = graph.edges(posn)
+        nbrs = graph[posn]
+        lookup = {}
         for i, nbr in enumerate(nbrs):
-            print i, graph[posn][nbr]['weight'], nbr
+            print i, graph[posn][nbr]['weight'], str(nbr)[:80]
+            lookup[i] = nbr
 #            print i, str(nbr)[:30]
         i = int(input('Go where?'))
-        posn = nbrs[i]
+        posn = lookup[i]
 
 if __name__ == '__main__':
     main()
